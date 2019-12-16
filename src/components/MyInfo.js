3803e5f5109566407a7951ba6b0f0111ae34f1a8
Jason Kempel <kempel.jason13@gmail.com>
2019-12-16 05:06:02 -0600

    initial commit

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+        <div class="container">
+            <h1>Jason Kempel</h1>
+            <p>I'm new to React, and I'm excited to learn more!</p>
+            <ul>
+                <li>Italy</li>
+                <li>Greece</li>
+                <li>Iceland</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Button from 'react-bootstrap/Button';
+
+function Navbar () {
+    return (
+        <nav class="nav">
+            <h4>Home</h4>
+            <h4>About Us</h4>
+        </nav>
+    )
+}
+
+
+export default Navbar
